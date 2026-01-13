a972b80b77699ba6aed11f35bd35c9ee7bdc93da
chaitanya <chaitanya@gmail.com>
2026-01-13 17:21:28 +0530

    added state locking

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "polaris-terraform-s3-latest"
+    key    = "polaris/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+
 provider "aws" {
   region = "ap-south-1"
 }
---
+++ s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "DemBasicS3" {
+  bucket = "polaris-terraform-s3-latest"
+}
